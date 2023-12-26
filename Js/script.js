2e8a905585a30f9a9296072587c4d77b29bc9c99
Kirito72-hub <ahmed.saudi1996@gmail.com>
2023-12-26 12:51:40 +0300

    -left the js clear to add swiper stuff later

--- Js/script.js
+++ Js/script.js
@@ -1,12 +1,2 @@
 // ==================swiper================
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 50,
-  loop: true,
-  grabCursor: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 
-});
